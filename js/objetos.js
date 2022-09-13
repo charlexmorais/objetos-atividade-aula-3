b5fc0cc4d9c7cd22fd7ae1ef3ae1b0ad656d6608
devcharle <charlexavier100@gmail.com>
2022-09-13 18:33:24 -0300

    enviando repositorio local para remoto

---
+++ js/objetos.js
@@ -0,0 +1,9 @@
+let apartamento={
+
+saladeestar:'mesa de centro',
+cozinha:'armarios',
+quartos:'Luminárias e abajures.',
+banheiro:1,
+areadeservico:'prateleiras suspensas.',
+}
+console.log("valor exibido",apartamento.quartos);
